635aab0eb4fe8f0dc44bc584c33a16a255a7073d
m0h1m3d <mohisham33@gmail.com>
2025-12-07 04:40:06 +0200

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
- return <h1>hello</h1>
+ return <div className="menu-container">
+  <Header />
+ </div>
 }
 
 export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './header.scss';
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>كباية <br /> مُقنن</h1>
+            <h2>Menu</h2>
+        </div>
+    )
+}
+
+export default Header
